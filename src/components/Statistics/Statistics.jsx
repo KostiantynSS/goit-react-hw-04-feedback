54e3d443c9cc87f8f41b3db52c3373281fcebf4c
KostiantynSS <storoghenko94@gmail.com>
2023-09-27 16:29:15 +0300

    Update Statistics.jsx

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -2,38 +2,42 @@ import css from './statistics.module.css';
 import { Component } from 'react';
 import PropTypes from 'prop-types';
 
-class Statistics extends Component {
-  static propTypes = {
-    good: PropTypes.number,
-    neutral: PropTypes.number,
-    bad: PropTypes.number,
-    total: PropTypes.number,
-    positivePercentage: PropTypes.number,
-  };
-  render() {
-    return (
-      <>
-        <h2>{this.props.title}</h2>
-        <div className={css.stat}>
-          <p>
-            Good: <span>{this.props.good}</span>
-          </p>
-          <p>
-            Neutral: <span>{this.props.neutral}</span>
-          </p>
-          <p>
-            Bad: <span>{this.props.bad}</span>
-          </p>
-          <p>
-            Total: <span>{this.props.total}</span>
-          </p>
-          <p>
-            Positive feedback: <span>{this.props.positivePercentage}%</span>
-          </p>
-        </div>
-      </>
-    );
-  }
-}
-
+const Statistics = ({
+  title,
+  good,
+  neutral,
+  bad,
+  total,
+  positivePercentage,
+}) => {
+  return (
+    <>
+      <h2>{title}</h2>
+      <div className={css.stat}>
+        <p>
+          Good: <span>{good}</span>
+        </p>
+        <p>
+          Neutral: <span>{neutral}</span>
+        </p>
+        <p>
+          Bad: <span>{bad}</span>
+        </p>
+        <p>
+          Total: <span>{total}</span>
+        </p>
+        <p>
+          Positive feedback: <span>{positivePercentage}%</span>
+        </p>
+      </div>
+    </>
+  );
+};
+Statistics.propTypes = {
+  good: PropTypes.number,
+  neutral: PropTypes.number,
+  bad: PropTypes.number,
+  total: PropTypes.number,
+  positivePercentage: PropTypes.number,
+};
 export default Statistics;
